4aed52c96bf2c9431b1b06da9e9d6cb06cfcc91f
Ana Finotelli <finotelli2004@gmail.com.br>
2022-09-30 20:44:37 -0300

    decisao

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int n1, n2;
+
+Console.WriteLine("Divisão de dois numeros");
+
+Console.WriteLine("Digite o primeiro numero:");
+n1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero:");
+n2 = Convert.ToInt32 (Console.ReadLine());
+
+bool valorInvalido = n2 == 2;
+if (valorInvalido)
+
+
+{
+
+    double resultado = n1 / n2;
+    Console.WriteLine($"{n1} dividido por {n2} é {resultado}");
+
+}
